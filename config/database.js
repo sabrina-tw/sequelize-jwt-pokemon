8a244432d58d28991a9d68bde80ff1f26f9875a1
Sabrina Sulong <sabrina.sulong@thoughtworks.com>
2021-08-27 10:02:50 +0800

    Set up Sequelize database config

---
+++ config/database.js
@@ -0,0 +1,30 @@
+module.exports = {
+  development: {
+    username: "postgres",
+    password: null,
+    database: "sequelize_jwt_pokemon_dev",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+  test: {
+    username: "postgres",
+    password: null,
+    database: "sequelize_jwt_pokemon_test",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+  production: {
+    username: process.env.PG_USER,
+    password: process.env.PG_PASS,
+    database: process.env.PG_DB_NAME,
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
+};
