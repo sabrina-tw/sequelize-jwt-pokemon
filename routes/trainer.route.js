c683e431f7a38dcc45f9c73430c1044d755d3565
Sabrina Sulong <sabrina.sulong@thoughtworks.com>
2021-08-27 16:44:05 +0800

    Protect trainers/search route with auth middleware
    Also implement login and logout routes

--- routes/trainer.route.js
+++ routes/trainer.route.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const db = require("../db/models/index");
 const { Op } = require("sequelize");
+const { auth } = require("../middleware/auth");
+const bcrypt = require("bcryptjs");
+const createJWTToken = require("../config/jwt");
 
 const router = express.Router();
 
@@ -26,7 +29,7 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-router.get("/search/:username", async (req, res, next) => {
+router.get("/search/:username", auth, async (req, res, next) => {
   try {
     const username = req.params.username;
     // [db.Sequelize.Op.iLike] allows you to do case-insensitive + partial querying
@@ -35,15 +38,61 @@ router.get("/search/:username", async (req, res, next) => {
       where: {
         username: { [Op.iLike]: "%" + username + "%" },
       },
-      attributes: {
-        exclude: ["password"],
-      },
     });
     res.send(trainer);
   } catch (err) {
     console.error(err);
     next(err);
   }
+});
+
+router.post("/login", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const trainer = await db.Trainer.findOne({
+      where: { username },
+      attributes: { include: ["password"] },
+    });
+
+    // return if Trainer does not exist
+    // message returned is intentionally vague for security reasons
+    if (!trainer) {
+      return res.status(422).json({ message: "Invalid username or password." });
+    }
+
+    // check if user input password matches hashed password in the db
+    const result = await bcrypt.compare(password, trainer.password);
+
+    if (!result) {
+      throw new Error("Login failed");
+    }
+
+    const token = createJWTToken(trainer.username);
+
+    // calculation to determine expiry date - this is up to your team to decide
+    const oneDay = 24 * 60 * 60 * 1000;
+    const oneWeek = oneDay * 7;
+    const expiryDate = new Date(Date.now() + oneWeek);
+
+    // you are setting the cookie here, and the name of your cookie is `token`
+    res.cookie("token", token, {
+      expires: expiryDate,
+      httpOnly: true, // client-side js cannot access cookie info
+      secure: true, // use HTTPS
+    });
+
+    res.send("You are now logged in!");
+  } catch (err) {
+    if (err.message === "Login failed") {
+      err.statusCode = 400;
+    }
+    next(err);
+  }
+});
+
+router.post("/logout", (req, res) => {
+  // clears the 'token' cookie from your browser
+  res.clearCookie("token").send("You are now logged out!");
 });
 
 module.exports = router;
