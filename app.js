337cb019aa43b9c5e298f59abd790af80a7fce09
Yu Lian Ng <yulian.ng@thoughtworks.com>
2021-08-27 14:02:08 +0800

    install cookie-parser,jsonwebtoken,bcryptjs and use cookie-parser in the app

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const app = express();
+
+const cookieParser = require("cookie-parser");
+
 const db = require("./db/models/index");
+
 const pokemonRouter = require("./routes/pokemon.route.js");
 
 app.use(express.json());
+app.use(cookieParser());
+
 app.use("/pokemons", pokemonRouter);
+
+
 
 db.sequelize.sync();
 
