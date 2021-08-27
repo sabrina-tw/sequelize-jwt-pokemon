b6a5630eb187be723d6c547816a6094edb723ab7
Sabrina Sulong <sabrina.sulong@thoughtworks.com>
2021-08-27 10:31:31 +0800

    Create Pokemon model and migration file

---
+++ db/migrations/20210827021809-create-pokemon.js
@@ -0,0 +1,38 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("Pokemons", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      japaneseName: {
+        type: Sequelize.STRING,
+      },
+      baseHp: {
+        type: Sequelize.INTEGER,
+      },
+      category: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("Pokemons");
+  },
+};
