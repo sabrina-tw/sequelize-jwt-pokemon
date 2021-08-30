231c31a1be261dfcddda6016cd8ba0b2273a4932
Sabrina Sulong <sabrina.sulong@thoughtworks.com>
2021-08-30 17:51:07 +0800

    Integrate React app into project
    To be deployed to Heroku as a full-stack application

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const path = require("path");
+const apiRouter = express.Router();
 
 const pokemonRouter = require("./routes/pokemon.route.js");
 const trainerRouter = require("./routes/trainer.route.js");
@@ -13,8 +15,15 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 
-app.use("/pokemons", pokemonRouter);
-app.use("/trainers", trainerRouter);
+app.use("/api", apiRouter);
+apiRouter.use("/pokemons", pokemonRouter);
+apiRouter.use("/trainers", trainerRouter);
+
+// allows us to deploy both front/backend to 1 Heroku app
+app.use(express.static(path.resolve("client", "build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.resolve("client", "build", "index.html"))
+);
 
 // default error handler
 app.use((err, req, res, next) => {
